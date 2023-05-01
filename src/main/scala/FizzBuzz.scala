c5791efb432b497e4d1c87c2f5616ff27d36aad5
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-05-01 20:03:46 +0100

    ♻️ change signature to int

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -3,8 +3,8 @@ package dev.wolfremium.kata
 import math.Integral.Implicits.infixIntegralOps
 
 class FizzBuzz:
-  def fromDigit(number: Number): String =
-    if number.intValue() % 3 == 0 then return "Fizz"
-    if number.intValue() % 5 == 0 then return "Buzz"
+  def fromDigit(number: Int): String =
+    if number % 3 == 0 then return "Fizz"
+    if number % 5 == 0 then return "Buzz"
     number.toString
 
