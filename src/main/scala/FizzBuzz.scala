a8f5c6facdaeb5f8768fe71abb039d3aa4ad434f
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-05-01 20:28:15 +0100

    ✅ given a buzz buzz number

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -8,7 +8,9 @@ class FizzBuzz:
     val isFizz = number % 3 == 0
     val isBuzz = number % 5 == 0
     val isFizzFizz = isFizz && number.toString.contains("3")
+    val isBuzzBuzz = isBuzz && number.toString.contains("5")
     if isFizzFizz then return "FizzFizz"
+    if isBuzzBuzz then return "BuzzBuzz"
     if isFizzBuzz then return "FizzBuzz"
     if isFizz then return "Fizz"
     if isBuzz then return "Buzz"
