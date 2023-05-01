47913b50846abb619bbda8579132a783231d0558
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-05-01 20:12:27 +0100

    ♻️ avoid magic numbers

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -4,8 +4,11 @@ import math.Integral.Implicits.infixIntegralOps
 
 class FizzBuzz:
   def fromDigit(number: Int): String =
-    if number % 15 == 0 then return "FizzBuzz"
-    if number % 3 == 0 then return "Fizz"
-    if number % 5 == 0 then return "Buzz"
+    val isFizzBuzz = number % 15 == 0
+    val isFizz = number % 3 == 0
+    val isBuzz = number % 5 == 0
+    if isFizzBuzz then return "FizzBuzz"
+    if isFizz then return "Fizz"
+    if isBuzz then return "Buzz"
     number.toString
 
