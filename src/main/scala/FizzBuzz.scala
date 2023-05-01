0eb7b921492a08f89edf3d4958c13ef4980a049e
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-05-01 19:58:50 +0100

    ✅ given a fizz number

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,5 +1,8 @@
 package dev.wolfremium.kata
 
+import math.Integral.Implicits.infixIntegralOps
+
 class FizzBuzz:
-  def fromDigit(number: Number): String = number.toString
+  def fromDigit(number: Number): String =
+    if number.intValue() % 3 == 0 then "Fizz" else number.toString
 
