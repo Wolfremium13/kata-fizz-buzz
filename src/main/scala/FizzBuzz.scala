af89b7975951e71b0ea9f59d8210e5b80484e27e
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-05-01 20:50:07 +0100

    ♻️ reorder conditions

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -4,15 +4,15 @@ import math.Integral.Implicits.infixIntegralOps
 
 class FizzBuzz:
   def fromDigit(number: Int): String =
-    val isFizzBuzz = number % 15 == 0
-    val isFizz = number % 3 == 0
-    val isBuzz = number % 5 == 0
     val containsThree = number.toString.contains("3")
-    val isFizzFizz = isFizz && containsThree
     val containsFive = number.toString.contains("5")
+    val isFizz = number % 3 == 0
+    val isFizzFizz = isFizz && containsThree
+    val isBuzz = number % 5 == 0
     val isBuzzBuzz = isBuzz && containsFive
-    val isFizzFizzBuzz = isFizz && containsThree && isBuzz
-    val isFizzBuzzBuzz = isBuzz && containsFive && containsThree
+    val isFizzBuzz = number % 15 == 0
+    val isFizzFizzBuzz = isFizz && containsThree && isBuzz || containsFive
+    val isFizzBuzzBuzz = isBuzz && containsFive && isFizz || containsThree
     if isFizzBuzzBuzz then return "FizzBuzzBuzz"
     if isFizzFizzBuzz then return "FizzFizzBuzz"
     if isFizzBuzz then return "FizzBuzz"
