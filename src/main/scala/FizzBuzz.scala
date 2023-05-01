12fe812c58c4f4122cdcf8c3000590f77a272caf
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-05-01 20:06:26 +0100

    ✅ given a fizz buzz number

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -4,6 +4,7 @@ import math.Integral.Implicits.infixIntegralOps
 
 class FizzBuzz:
   def fromDigit(number: Int): String =
+    if number % 15 == 0 then return "FizzBuzz"
     if number % 3 == 0 then return "Fizz"
     if number % 5 == 0 then return "Buzz"
     number.toString
